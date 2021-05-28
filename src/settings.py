dfa7b594530b3cdb5fc9328e1ea3b4853c3b7aaf
bulldra <bullda@gmail.com>
2021-05-29 07:59:39 +0900

    read_text 削除

--- src/settings.py
+++ src/settings.py
@@ -9,13 +9,6 @@ def load_json(path):
     with open(_build_path(path), 'r') as conf:
         return json.load(conf)
 
-def load_text(path):
-    with open(_build_path(path), 'r') as conf:
-        result = []
-        for line in conf.read().splitlines():
-            result.append(line)
-        return result
-
 def _build_path(path):
     abspath = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
     abspath = os.path.abspath(abspath)
@@ -26,4 +19,3 @@ def _build_path(path):
 
 settings_dict = load_json('../config/settings.json')
 kindle_lib_path = settings_dict['kindle_lib_path']
-
