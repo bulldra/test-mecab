41f6170fffc8d398c409cdd4ae08cd1e68491143
bulldra <bullda@gmail.com>
2021-05-29 17:33:46 +0900

    フォーマット修正

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -4,19 +4,23 @@ __version__ = "0.1.0"
 
 import settings
 
+
 def test_config():
-    assert settings.settings_dict['logfile'] == '../log/info.log'
+    assert(settings.settings_dict['logfile'] == '../log/info.log')
+
 
 def test_load_json():
     settings_dict = settings.load_json('../config/settings.json')
-    assert settings_dict['logfile'] == '../log/info.log'
+    assert(settings_dict['logfile'] == '../log/info.log')
+
 
 def test_build_path():
     path = settings._build_path('../config/settings.json')
-    assert path == '/data/config/settings.json'
+    assert(path == '/data/config/settings.json')
+
 
 def test_notfilepath_json():
     try:
         settings.load_json('../config/settings.jso')
     except FileNotFoundError as e:
-        assert str(e) == '/data/config/settings.jso'
+        assert(str(e) == '/data/config/settings.jso')
