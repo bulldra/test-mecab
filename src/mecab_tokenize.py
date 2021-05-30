c4dbd7329d7624ac89386f15304103132da542f4
bulldra <bullda@gmail.com>
2021-05-30 15:38:20 +0900

    dataframe化

--- src/mecab_tokenize.py
+++ src/mecab_tokenize.py
@@ -6,7 +6,7 @@ import argparse
 import logzero
 import settings
 import MeCab
-import collections
+import pandas
 
 
 class MecabTokenize:
@@ -17,6 +17,18 @@ class MecabTokenize:
         )
         self.logger = logzero.logger
         self.mecab = MeCab.Tagger(settings.mecab_param)
+        pandas.set_option('display.max_rows', None)
+
+    def tokenize(self, text):
+        p = [li.split('\t') for li in self.mecab.parse(text).splitlines()]
+        p = [[li[0]] + li[1].split('-') + [None] for li in p if len(li) == 2]
+        p = [{'term': li[0], 'info1': li[1], 'info2': li[2]} for li in p]
+        return pandas.DataFrame(p)
+
+    def freq(self, text, count=1):
+        s = self.tokenize(text).groupby(['term', 'info1', 'info2']).size()
+        s = s[s >= count].sort_values(ascending=False)
+        return s.to_frame('freq').reset_index()
 
     def main(self, args):
         self.logger.info(f'{__file__} {__version__} {args}')
@@ -27,13 +39,6 @@ class MecabTokenize:
         freq_list = self.freq(args.args1)
         print('\n単語出現リスト')
         print(freq_list)
-
-    def tokenize(self, text):
-        p = [li.split('\t') for li in self.mecab.parse(text).splitlines()]
-        return [{'term': li[0], 'pos': li[1]} for li in p if len(li) == 2]
-
-    def freq(self, text):
-        return collections.Counter([t['term'] for t in self.tokenize(text)])
 
 
 if(__name__ == '__main__'):
--- tests/test_mecab_tokenize.py
+++ tests/test_mecab_tokenize.py
@@ -3,44 +3,39 @@
 __version__ = "0.1.0"
 
 import mecab_tokenize
-import collections
+import pandas
 
 
 def test_tokenize1():
-    text = '私はpythonでmecabを扱うテストをしています。'
-    expect = [
-        {'term': '私', 'pos': '名詞-代名詞-一般'},
-        {'term': 'は', 'pos': '助詞-係助詞'},
-        {'term': 'で', 'pos': '助詞-格助詞-一般'},
-        {'term': 'mecab', 'pos': '名詞-固有名詞-一般'},
-        {'term': 'を', 'pos': '助詞-格助詞-一般'},
-        {'term': '扱う', 'pos': '動詞-自立'},
-        {'term': 'テスト', 'pos': '名詞-サ変接続'},
-        {'term': 'を', 'pos': '助詞-格助詞-一般'},
-        {'term': 'し', 'pos': '動詞-自立'},
-        {'term': 'て', 'pos': '助詞-接続助詞'},
-        {'term': 'い', 'pos': '動詞-非自立'},
-        {'term': 'ます', 'pos': '助動詞'},
-        {'term': '。', 'pos': '記号-句点'},
-    ]
+    text = 'テストする'
+    expect = pandas.DataFrame([
+        {'term': 'テスト', 'info1': '名詞', 'info2': 'サ変接続'},
+        {'term': 'する', 'info1': '動詞', 'info2': '自立'},
+    ])
     actual = mecab_tokenize.MecabTokenize().tokenize(text)
-    assert(expect == actual)
+    pandas.util.testing.assert_frame_equal(expect, actual)
 
 
 def test_tokenize2():
-    text = 'テストする'
-    expect = [
-        {'term': 'テスト', 'pos': '名詞-サ変接続'},
-        {'term': 'する', 'pos': '動詞-自立'},
-    ]
+    text = 'mecab mecabテストmecabテストツール'
+    expect = pandas.DataFrame([
+        {'term': 'mecab', 'info1': '名詞', 'info2': '固有名詞'},
+        {'term': 'mecab', 'info1': '名詞', 'info2': '固有名詞'},
+        {'term': 'テスト', 'info1': '名詞', 'info2': 'サ変接続'},
+        {'term': 'mecab', 'info1': '名詞', 'info2': '固有名詞'},
+        {'term': 'テスト', 'info1': '名詞', 'info2': 'サ変接続'},
+        {'term': 'ツール', 'info1': '名詞', 'info2': '一般'},
+    ])
     actual = mecab_tokenize.MecabTokenize().tokenize(text)
-    assert(expect == actual)
+    pandas.util.testing.assert_frame_equal(expect, actual)
 
 
 def test_freq():
     text = 'mecab mecabテストmecabテストツール'
-    expect = collections.Counter(
-        {'mecab': 3, 'テスト': 2, 'ツール': 1}
-    )
+    expect = pandas.DataFrame([
+        {'term': 'mecab', 'info1': '名詞', 'info2': '固有名詞', 'freq': 3},
+        {'term': 'テスト', 'info1': '名詞', 'info2': 'サ変接続', 'freq': 2},
+        {'term': 'ツール', 'info1': '名詞', 'info2': '一般', 'freq': 1},
+    ])
     actual = mecab_tokenize.MecabTokenize().freq(text)
-    assert(expect == actual)
+    pandas.util.testing.assert_frame_equal(expect, actual)
